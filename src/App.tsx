98e17e42f9ed91ad9946d6d9e649311fa2f109e3
phil92 <philip.chan92@protonmail.com>
2022-08-29 11:40:14 -0500

    add background image and swap input to assets, use CssBaseLine instead of Paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import { defaultTheme } from './themes';
 import WalletConnector from './components/wallet-connector/WalletConnector';
 import Swap from './components/swap/Swap';
 
-
 import { Paper, Grid } from '@mui/material/';
 
 const App: React.FunctionComponent = () => {
-
     return (
         <ThemeProvider theme={defaultTheme}>
-            <Paper>
-                <Grid sx={{ height: '100vh' }}>
-                    <WalletConnector />
-                    <Swap/>
-                </Grid>
-            </Paper>
+            <CssBaseline />
+            <Grid sx={{ height: '100vh' }}>
+                <WalletConnector />
+                <Swap />
+            </Grid>
         </ThemeProvider>
     );
 };
--- src/themes.ts
+++ src/themes.ts
@@ -1,42 +1,55 @@
-import { createTheme } from "@mui/material/styles";
-
+import { createTheme } from '@mui/material/styles';
+import dexBackground from '../src/assets/dex_background.png';
 export const defaultTheme = createTheme({
-  palette: {
-    primary: {
-      main: "#fff",
-    },
-    text: {
-      primary: "rgb(255, 255, 255)",
-      secondary: "rgba(255, 255, 255, 0.7)",
-    },
-  },
-  typography: {
-    fontFamily: "Signika Negative",
-    fontWeightLight: 300,
-    fontWeightBold: 700,
-  },
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root: {
-          background:
-            "linear-gradient(180deg, rgba(166,73,133,1) 0%, rgba(96,45,156,1) 23%, rgba(77,38,143,1) 39%, rgba(45,40,147,1) 56%, rgba(42,74,150,1) 73%, rgba(73,171,176,1) 100%);",
-          borderRadius: 0,
+    palette: {
+        primary: {
+            main: '#fff',
         },
-      },
-    },
-    MuiButtonBase: {
-      defaultProps: {
-        disableRipple: true,
-      },
-    },
-    MuiAutocomplete: {
-      styleOverrides: {
-        paper: {
-          background: "transparent",
-          boxShadow: "none",
+        text: {
+            primary: 'rgb(255, 255, 255)',
+            secondary: 'rgba(255, 255, 255, 0.7)',
         },
-      },
     },
-  },
+    typography: {
+        fontFamily: 'Signika Negative',
+        fontWeightLight: 300,
+        fontWeightBold: 700,
+    },
+
+    components: {
+        // MuiPaper: {
+        //   styleOverrides: {
+        //     root: {
+        //       background: `url(${dexBackground})`,
+        //       // background:
+        //       //   "linear-gradient(180deg, rgba(166,73,133,1) 0%, rgba(96,45,156,1) 23%, rgba(77,38,143,1) 39%, rgba(45,40,147,1) 56%, rgba(42,74,150,1) 73%, rgba(73,171,176,1) 100%);",
+        //       borderRadius: 0,
+        //     },
+        //   },
+        // },
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    backgroundImage: `url(${dexBackground})`,
+                     backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center center',
+                },
+            },
+        },
+
+        MuiButtonBase: {
+            defaultProps: {
+                disableRipple: true,
+            },
+        },
+        MuiAutocomplete: {
+            styleOverrides: {
+                paper: {
+                    background: 'transparent',
+                    boxShadow: 'none',
+                },
+            },
+        },
+    },
 });
