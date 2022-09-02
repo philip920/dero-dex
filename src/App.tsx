eb1248c0a8ce525b5009eb208ac33b81c7d13981
phil92 <philip.chan92@protonmail.com>
2022-09-02 15:22:28 -0500

    add DeroBrifgeApi, create functions for rpc calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
 import { defaultTheme } from './themes';
 import WalletConnector from './components/wallet-connector/WalletConnector';
 import Swap from './components/swap/Swap';
-
-import { Grid } from '@mui/material/';
+import DeroBridgeApi from './DeroBridgeApi';
+import to from 'await-to-js';
 
 const App: React.FunctionComponent = () => {
+    const deroBridgeApiRef = useRef();
+    const [bridgeInitText, setBridgeInitText] = useState('');
+
+    useEffect(() => {
+        const load = async () => {
+            // @ts-ignore
+            deroBridgeApiRef.current = new DeroBridgeApi();
+            const deroBridgeApi = deroBridgeApiRef.current;
+            // @ts-ignore
+            const [err] = await to(deroBridgeApi.init());
+            if (err) {
+                setBridgeInitText('failed to connect to extension');
+            } else {
+                setBridgeInitText('connected to extension');
+            }
+        };
+
+        window.addEventListener('load', load);
+        return () => window.removeEventListener('load', load);
+    }, []);
+
     return (
         <ThemeProvider theme={defaultTheme}>
             <CssBaseline />
--- src/components/common/rpc-calls.ts
+++ src/components/common/rpc-calls.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import React, { useCallback } from 'react';
+import to from 'await-to-js';
 
 const DEX_URL = 'http://localhost:3009/';
 
@@ -8,164 +10,179 @@ const DEX_SCID =
 const DERO_SCID =
     'd5cb7dcb6dfa70310053bb0ddebca65494471771d12ff133711f29792d17acae';
 
-export const addLiquidity = async (baseAsset: string, quoteAsset: string) => {
-    const { data } = await axios.post(DEX_URL, {
-        jsonrpc: '2.0',
-        id: '1',
-        method: 'transfer',
-        params: {
-            scid: DEX_SCID,
-            ringsize: 2,
-            sc_rpc: [
-                {
-                    name: 'entrypoint',
-                    datatype: 'S',
-                    value: 'AddLiquidity',
-                },
-                {
-                    name: 'baseAsset',
-                    datatype: 'H',
-                    value: baseAsset,
-                },
-                {
-                    name: 'qouteAsset',
-                    datatype: 'H',
-                    value: quoteAsset,
-                },
-            ],
-            transfers: [
-                {
-                    scid: 'dero-wrapped-token-1-scid',
-                    burn: 10000000,
-                },
-                {
-                    scid: 'dero-wrapped-token-2-scid',
-                    burn: 10000000,
-                },
-            ],
-        },
-    });
+export const addLiquidity = useCallback(
+    async (
+        deroBridgeApiRef: any,
+        baseAsset: string,
+        quoteAsset: string,
+        amountToSend: number
+    ) => {
+        const deroBridgeApi = deroBridgeApiRef.current;
+        const [err, res] = await to(
+            deroBridgeApi.wallet('transfer', {
+                scid: DEX_SCID,
+                ringsize: 2,
+                sc_rpc: [
+                    {
+                        name: 'entrypoint',
+                        datatype: 'S',
+                        value: 'AddLiquidity',
+                    },
+                    {
+                        name: 'baseAsset',
+                        datatype: 'H',
+                        value: baseAsset,
+                    },
+                    {
+                        name: 'qouteAsset',
+                        datatype: 'H',
+                        value: quoteAsset,
+                    },
+                ],
+                transfers: [
+                    {
+                        scid: 'dero-wrapped-token-1-scid',
+                        burn: amountToSend,
+                    },
+                    {
+                        scid: 'dero-wrapped-token-2-scid',
+                        burn: amountToSend,
+                    },
+                ],
+            })
+        );
 
-    return data;
-};
+        if (err) alert(err.message);
+        else alert(JSON.stringify(res));
+    },
+    []
+);
 
-export const removeLiquidity = async (
-    baseAsset: string,
-    quoteAsset: string
-) => {
-    const { data } = await axios.post(DEX_URL, {
-        jsonrpc: '2.0',
-        id: '1',
-        method: 'transfer',
-        params: {
-            scid: DEX_SCID,
-            ringsize: 2,
-            sc_rpc: [
-                {
-                    name: 'entrypoint',
-                    datatype: 'S',
-                    value: 'RemoveLiquidity',
-                },
-                {
-                    name: 'baseAsset',
-                    datatype: 'H',
-                    value: baseAsset,
-                },
-                {
-                    name: 'qouteAsset',
-                    datatype: 'H',
-                    value: quoteAsset,
-                },
-            ],
-            transfers: [
-                {
-                    scid: DEX_SCID,
-                    burn: 1000,
-                },
-            ],
-        },
-    });
+export const removeLiquidity = useCallback(
+    async (
+        deroBridgeApiRef: any,
+        baseAsset: string,
+        quoteAsset: string,
+        amountToConvert: number
+    ) => {
+        const deroBridgeApi = deroBridgeApiRef.current;
+        const [err, res] = await to(
+            deroBridgeApi.wallet('transfer', {
+                scid: DEX_SCID,
+                ringsize: 2,
+                sc_rpc: [
+                    {
+                        name: 'entrypoint',
+                        datatype: 'S',
+                        value: 'RemoveLiquidity',
+                    },
+                    {
+                        name: 'baseAsset',
+                        datatype: 'H',
+                        value: baseAsset,
+                    },
+                    {
+                        name: 'qouteAsset',
+                        datatype: 'H',
+                        value: quoteAsset,
+                    },
+                ],
+                transfers: [
+                    {
+                        scid: DEX_SCID,
+                        burn: amountToConvert,
+                    },
+                ],
+            })
+        );
 
-    return data;
-};
+        if (err) alert(err.message);
+        else alert(JSON.stringify(res));
+    },
+    []
+);
 
-export const swap = async (
-    fromAsset: string,
-    toAsset: string,
-    amount: number
-) => {
-    const { data } = await axios.post(DEX_URL, {
-        jsonrpc: '2.0',
-        id: '1',
-        method: 'transfer',
-        params: {
-            scid: DEX_SCID,
-            ringsize: 2,
-            sc_rpc: [
-                {
-                    name: 'entrypoint',
-                    datatype: 'S',
-                    value: 'Swap',
-                },
-                {
-                    name: 'fromAsset',
-                    datatype: 'H',
-                    value: fromAsset,
-                },
-                {
-                    name: 'toAsset',
-                    datatype: 'H',
-                    value: toAsset,
-                },
-            ],
-            transfers: [
-                {
-                    scid: DERO_SCID,
-                    burn: amount,
-                },
-            ],
-        },
-    });
+export const swap = useCallback(
+    async (
+        deroBridgeApiRef: any,
+        fromAsset: string,
+        toAsset: string,
+        amountToSwap: number
+    ) => {
+        const deroBridgeApi = deroBridgeApiRef.current;
+        const [err, res] = await to(
+            deroBridgeApi.wallet('transfer', {
+                scid: DEX_SCID,
+                ringsize: 2,
+                sc_rpc: [
+                    {
+                        name: 'entrypoint',
+                        datatype: 'S',
+                        value: 'Swap',
+                    },
+                    {
+                        name: 'fromAsset',
+                        datatype: 'H',
+                        value: fromAsset,
+                    },
+                    {
+                        name: 'toAsset',
+                        datatype: 'H',
+                        value: toAsset,
+                    },
+                ],
+                transfers: [
+                    {
+                        scid: DERO_SCID,
+                        burn: amountToSwap,
+                    },
+                ],
+            })
+        );
 
-    return data;
-};
+        if (err) alert(err.message);
+        else alert(JSON.stringify(res));
+    },
+    []
+);
 
-export const widthdrawAssetFees = async (asset: string) => {
-    const { data } = await axios.post(DEX_URL, {
-        jsonrpc: '2.0',
-        id: '1',
-        method: 'transfer',
-        params: {
-            scid: DEX_SCID,
-            ringsize: 2,
-            sc_rpc: [
-                {
-                    name: 'entrypoint',
-                    datatype: 'S',
-                    value: 'WithdrawAssetFees',
-                },
-                {
-                    name: 'asset',
-                    datatype: 'H',
-                    value: asset,
-                },
-            ],
-        },
-    });
+export const widthdrawAssetFees = useCallback(
+    async (deroBridgeApiRef: any, asset: string) => {
+        const deroBridgeApi = deroBridgeApiRef.current;
+        const [err, res] = await to(
+            deroBridgeApi.wallet('transfer', {
+                scid: DEX_SCID,
+                ringsize: 2,
+                sc_rpc: [
+                    {
+                        name: 'entrypoint',
+                        datatype: 'S',
+                        value: 'WithdrawAssetFees',
+                    },
+                    {
+                        name: 'asset',
+                        datatype: 'H',
+                        value: asset,
+                    },
+                ],
+            })
+        );
 
-    return data;
-};
+        if (err) alert(err.message);
+        else alert(JSON.stringify(res));
+    },
+    []
+);
 
-export const readDexVariables = async () => {
-    const { data } = await axios.post(DEX_URL, {
-        jsonrpc: '2.0',
-        id: '1',
-        method: 'DERO.GetSC',
-        params: {
+export const readDexVariables = useCallback(async (deroBridgeApiRef: any) => {
+    const deroBridgeApi = deroBridgeApiRef.current;
+    const [err, res] = await to(
+        deroBridgeApi.wallet('DERO.GetSC', {
             scid: DEX_SCID,
             variables: true,
-        },
-    });
+        })
+    );
 
-    return data;
-};
+    if (err) alert(err.message);
+    else alert(JSON.stringify(res));
+}, []);
